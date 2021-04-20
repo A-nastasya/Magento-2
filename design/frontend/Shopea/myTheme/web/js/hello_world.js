418a01ea7916966a0ff5de7e7c697e7293a2ed3d
Anastasia <anastasiazvonnikova@gmail.com>
2021-04-20 14:30:54 +0300

    console output from admin panel

--- design/frontend/Shopea/myTheme/web/js/hello_world.js
+++ design/frontend/Shopea/myTheme/web/js/hello_world.js
@@ -4,6 +4,7 @@ define([
         "use strict";
         return function(config, element) {
           console.log('Hello world!');
+          console.log(config.sayHI);
              
         }
     }
